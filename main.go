5bf9e8b67c59b55afbc4423cf7ff91f2ebce7bf0
liuliang <satng@qq.com>
2019-08-08 17:38:17 +0800

    fix taos data

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"github.com/satng/sensors-gateway-grpc/pb"
-	_ "github.com/satng/sensors-gateway-grpc/taosSql"
-	"github.com/satng/sensors-gateway-grpc/taosTool"
+	"strings"
+
+	//_ "github.com/satng/sensors-gateway-grpc/taosSql"
 	"google.golang.org/grpc"
 	"log"
 	"net"
@@ -17,18 +20,34 @@ const (
 type server struct {
 }
 
+const stmtSensorSql = `INSERT INTO d_%s USING sensors_data TAGS ('%s','%s','%s') VALUES`
+
 func (s *server) DataPush(ctx context.Context, in *pb.SensorRequest) (*pb.SensorReply, error) {
 	log.Printf("Received Header: %v,%v,%v", in.GetDeviceId(), in.GetRecordId(), in.GetSensorType())
-	//log.Printf("Received Data: %v", in.GetDataStr())
+
+	if in.GetSensorType() != "gps" {
+		stmtSensorSqlHeader := fmt.Sprintf(stmtSensorSql, in.GetDeviceId(), in.GetDeviceId(), in.GetRecordId(), in.GetSensorType())
+		var stmtSensorSqlContext bytes.Buffer
+		for _, item := range in.GetDataStr() {
+			//1565253030508;0.0000;9.8100;0.0000;57207591632172;1565253030488.63
+			items := strings.Split(item, ";")
+			ts_sensor := items[5]
+			stmtSensorSqlContext.WriteString(fmt.Sprintf(`(%v,%v,%v,%v,%v,%v)`, ts_sensor[:strings.LastIndex(ts_sensor, ".")], items[0], items[1], items[2], items[3], items[4]))
+		}
+
+		stmtSensorSql := fmt.Sprintf(`%s %s;`, stmtSensorSqlHeader, stmtSensorSqlContext.String())
+		log.Printf("Received Data: %v", stmtSensorSql)
+	}
+
 	return &pb.SensorReply{Message: "Hello " + in.DeviceId}, nil
 }
 
 func main() {
 
 	//test taos db
-	if taosTool.Test() {
-		return
-	}
+	//if taosTool.Test() {
+	//	return
+	//}
 
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
